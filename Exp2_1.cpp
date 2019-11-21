7a6f833a3b4e6b60c5fb3a06db7b659421c27087
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 01:48:16 -0500

    Create Exp2_1.cpp

---
+++ Exp2_1.cpp
@@ -0,0 +1,8 @@
+// Mon premier programme en C++
+#include <iostream>using namespace std;
+main()
+{
+  cout << "Bienvenue au cours Programmer en C++"<< endl;
+  return 0; 
+  // Indique que le programme se termine avec succès
+}
