87c81a08656860fb42072ed5bccfcdfe06b21c5f
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 02:14:49 -0500

    Create So2_9.cpp

---
+++ So2_9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*#include <conio.h>*/
+main ()
+{
+int nbre_secondes_par_heure;
+nbre_secondes_par_heure = 60 * 60;
+cout  <<"Une  heure  compte  :  "<<nbre_secondes_par_heure<<  " secondes" <<endl;
+nbre_secondes_par_heure = (60-20) * 60;
+cout <<"40 minutes, il y a : " <<nbre_secondes_par_heure<< " secondes" <<endl;
+/*getch ();*/
+return 0;
+}
