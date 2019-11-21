05028d38004650539ad8ac1139b7770fb3b3eb3c
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 02:09:47 -0500

    Create So2_3.cpp

---
+++ So2_3.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+main ()
+{
+cout << "1";cout << "2";
+cout << "3";
+cout << "4" ;cout << "a";
+cout << "b";
+cout << "c";
+cout << "d" <<endl;
+return 0;
+}
